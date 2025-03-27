6b85e51843a640e31abfaf2a47f3e9bcf5fbd45d
wom2025 <hmarien7@gmail.com>
2025-03-27 08:14:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -168,3 +168,8 @@ def download_cbmp_figure(n, fig):
 
 if __name__ == '__main__':
     app.run(debug=False, port=8058)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
